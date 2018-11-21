222b3921b2748b61bc51d81cc63a9e4b47349dec
Gunnar Hafdal <gunnar@gunnarhafdal.com>
2018-11-21 16:55:38 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const fs = require('fs');
+const stringify = require('csv-stringify/lib/sync');
+const publish = require('./publish');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.post('/add', (req, res) => {
+  const lykill = req.body.lykill;
+  
+  if(lykill !== process.env.LYKILL) {
+    res.sendStatus(403);
+    return
+  }
+
+  const title = req.body.title;
+  const url = req.body.url;
+  const time = new Date().getTime();
+
+  
+  
+  try {
+    let line = stringify([[time,url,title]]);
+
+    fs.appendFileSync('bookmarks.csv', line, 'utf8');
+
+    console.log(line);
+    publish.publish();
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(200);
+  }
+});
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
+
---
+++ publish.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
+const mustache = require('mustache');
+
+function sortByDate(a, b) {
+  if (a[0] < b[0]) return 1;
+  if (a[0] > b[0]) return -1;
+  return 0;
+}
+
+function ordinal_suffix_of(i) {
+  var j = i % 10,
+      k = i % 100;
+  if (j == 1 && k != 11) {
+      return i + "st";
+  }
+  if (j == 2 && k != 12) {
+      return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+      return i + "rd";
+  }
+  return i + "th";
+}
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const publish = () => {
+  let bookmarks = fs.readFileSync('bookmarks.csv', 'utf8');
+  let parsedData = parse(bookmarks);
+  parsedData = parsedData.sort(sortByDate)
+  //console.log(parsedData);
+
+  parsedData.forEach(line => {
+    let date = new Date(parseInt(line[0]));
+    
+    let weekday = weekdays[date.getUTCDay()];
+    let dayOfMonth = ordinal_suffix_of(date.getUTCDate());
+    let month = months[date.getUTCMonth()];
+    let year = date.getUTCFullYear();
+
+    let hours = date.getUTCHours();
+    hours = hours < 10 ? `0${hours}` : hours;
+    let minutes = date.getUTCMinutes();
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    let dateLine = `${weekday} ${dayOfMonth} of ${month} ${year} @ ${hours}:${minutes}`;
+
+    line.push(dateLine);
+
+    let url = line[1];
+    url = url.replace('https://', '');
+    url = url.replace('http://', '');
+    url = url.split('/')[0];
+
+    line.push(url);
+  });
+
+  let template = fs.readFileSync('template.mustache', 'utf8');
+  let html = mustache.to_html(template, {context: parsedData });
+  fs.writeFileSync('public/index.html', html, 'utf8');
+}
+
+
+
+module.exports = {
+  publish: publish
+}
