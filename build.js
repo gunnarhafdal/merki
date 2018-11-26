cd8a7a0cd0a9379baf8795c83bb8ef255ce5d43b
Gunnar Hafdal <gunnar.hafdal@valtech.com>
2018-11-26 16:34:58 +0100

    Add the start of a bookmarklet to the build process.

--- build.js
+++ build.js
@@ -1,3 +1,13 @@
-const publish = require('./publish');
+require('dotenv').config();
+const fs = require('fs');
+const mustache = require('mustache');
 
+console.log("Publishing 'bookmarks.csv' to 'public/index.html'.");
+const publish = require('./publish');
 publish.publish();
+
+console.log("Creating 'bookmarklet.js'.");
+let template = fs.readFileSync('bookmarklet.mustache', 'utf8');
+let html = mustache.to_html(template, {secret: process.env.SECRET, domain: process.env.DOMAIN });
+fs.writeFileSync('bookmarklet.js', html, 'utf8');
+console.log("Done.");
